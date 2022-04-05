5aac47f96f5f4cf5cadf20c8ccaf4af6836f733b
shungo11260 <shungotaniguchi1120@gmail.com>
2022-04-05 06:03:24 +0000

    feat: add todo list page; modify port setting

---
+++ pages/toDoLists.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ toDos }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const toDos = await app.$axios.$get('http://localhost:8080/api/to_dos')
+    return { toDos }
+  },
+}
+</script>
