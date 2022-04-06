91f36c533bce2eac08fe33530d5d9084acf328f9
shungo11260 <shungotaniguchi1120@gmail.com>
2022-04-06 05:25:34 +0000

    refactor: modify route of task; api url

---
+++ pages/boards/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ tasks }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const tasks = await app.$axios.$get(process.env.API_BASE + '/api/tasks')
+    return { tasks }
+  },
+}
+</script>
