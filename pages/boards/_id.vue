50c500f08bb74795ef1c1dd8fa01fdc8a2855e4a
shungo11260 <shungotaniguchi1120@gmail.com>
2022-04-07 02:18:58 +0000

    feat: enable showing lists in a board

--- pages/boards/_id.vue
+++ pages/boards/_id.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
-    {{ board }}
-  </div>
+  <v-container>
+    <v-card v-for="list in res.lists" :key="list.id">
+      <v-card-title>{{ list.title }}</v-card-title>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
   async asyncData({ app, params }) {
-    const board = await app.$axios.$get(
+    const res = await app.$axios.$get(
       process.env.API_BASE + '/api/boards/' + params.id
     )
-    return { board }
+    return { res }
   },
 }
 </script>
